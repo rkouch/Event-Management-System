1137579e6df744d3f0ee710f95fb4be176a91c22
Tom Harrison-Brown <113866206+tomharrisonbrown@users.noreply.github.com>
2022-09-23 23:41:21 +1000

    Configured log4j

--- backend/src/main/java/tickr/Main.java
+++ backend/src/main/java/tickr/Main.java
@@ -1,7 +1,16 @@
 package tickr;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        logger.debug("Test debug!");
+        logger.info("Test info!");
+        logger.warn("Test warning!");
+        logger.error("Test error!");
+        logger.fatal("Test fatal!");
     }
 }
