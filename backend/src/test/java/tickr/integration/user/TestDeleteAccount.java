40d0203a3588c4ad0ece200f17b5df96dab728c5
Jerry Hu <68409237+JerryHu01@users.noreply.github.com>
2022-10-10 04:20:06 +1100

    fixed minor error

--- backend/src/test/java/tickr/integration/user/TestDeleteAccount.java
+++ backend/src/test/java/tickr/integration/user/TestDeleteAccount.java
@@ -13,7 +13,7 @@ import tickr.application.serialised.requests.UserLoginRequest;
 import tickr.application.serialised.requests.UserRegisterRequest;
 import tickr.application.serialised.responses.AuthTokenResponse;
 import tickr.application.serialised.responses.ViewProfileResponse;
-import tickr.integration.HTTPHelper;
+import tickr.util.HTTPHelper;
 import tickr.persistence.DataModel;
 import tickr.persistence.HibernateModel;
 import tickr.server.Server;
