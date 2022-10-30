0435dc0ba893e04e040aa894e0c62e80be72bd8b
Tom Harrison-Brown <113866206+tomharrisonbrown@users.noreply.github.com>
2022-10-31 02:06:43 +1100

    Implemented stripe api webhook handling

--- backend/src/main/java/tickr/application/apis/purchase/StripeAPI.java
+++ backend/src/main/java/tickr/application/apis/purchase/StripeAPI.java
@@ -1,14 +1,19 @@
 package tickr.application.apis.purchase;
 
+import com.google.gson.JsonSyntaxException;
 import com.stripe.Stripe;
+import com.stripe.exception.SignatureVerificationException;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Event;
 import com.stripe.model.checkout.Session;
+import com.stripe.net.Webhook;
 import com.stripe.param.checkout.SessionCreateParams;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import tickr.application.TickrController;
 import tickr.persistence.ModelSession;
+import tickr.server.exceptions.BadRequestException;
+import tickr.server.exceptions.ForbiddenException;
 
 public class StripeAPI implements IPurchaseAPI {
     static final Logger logger = LogManager.getLogger();
@@ -31,13 +36,40 @@ public class StripeAPI implements IPurchaseAPI {
     public String registerOrder (IOrderBuilder builder) {
         var session = ((PaymentSessionBuilder)builder).build();
 
-        return session.getUrl();
+        var url = session.getUrl();
+        logger.info("Created stripe session with url {}!", url);
+        return url;
     }
 
     @Override
     public void handleWebhookEvent (TickrController controller, ModelSession session, String requestBody, String sigHeader) {
         logger.info("Webhook event: \n{}", requestBody);
+        Event event = null;
+        try {
+            event = Webhook.constructEvent(requestBody, sigHeader, webhookSecret);
+        } catch (JsonSyntaxException e) {
+            throw new BadRequestException("Invalid payload!");
+        } catch (SignatureVerificationException e) {
+            throw new BadRequestException("Invalid signature!");
+        }
+
+        if ("checkout.session.completed".equals(event.getType())) {
+            event.getDataObjectDeserializer()
+                    .getObject()
+                    .ifPresent(s -> completePayment(controller, session, (Session) s));
+        }
     }
+
+    private void completePayment (TickrController controller, ModelSession session, Session stripeSession) {
+        var metadata = stripeSession.getMetadata();
+        if (!metadata.containsKey("reserve_id")) {
+            throw new RuntimeException("Invalid metadata!");
+        }
+
+        var orderId = metadata.get("reserve_id");
+        controller.ticketPurchaseSuccess(session, orderId);
+    }
+
 
     private void doPayment (TickrController controller, ModelSession session, String requestBody, String sigHeader) {
         Event event = null;
@@ -48,7 +80,7 @@ public class StripeAPI implements IPurchaseAPI {
 
     @Override
     public String getSignatureHeader () {
-        return null;
+        return "Stripe-Signature";
     }
 
     private static class PaymentSessionBuilder implements IOrderBuilder {
