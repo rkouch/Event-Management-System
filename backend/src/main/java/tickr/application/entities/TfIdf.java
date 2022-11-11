e6909d782c57da0ee16066ac238d7723e2c094ff
Tom Harrison-Brown <113866206+tomharrisonbrown@users.noreply.github.com>
2022-11-12 10:45:38 +1100

    Added tests for term tfidfs

---
+++ backend/src/main/java/tickr/application/entities/TfIdf.java
@@ -0,0 +1,76 @@
+package tickr.application.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tf_idf")
+public class TfIdf {
+    @EmbeddedId
+    private TermId termId;
+
+    @Column(name = "term_freq")
+    private double termFreq;
+
+    @Column(name = "document_count")
+    private int documentCount;
+
+    public TfIdf () {
+
+    }
+
+    public TfIdf (DocumentTerm term, Event event, double termFreq) {
+        this.termId = new TermId(term, event);
+        this.termFreq = termFreq;
+        this.documentCount = term.getTermCount();
+    }
+
+    public double getTfIdf () {
+        return 0;
+    }
+
+    public DocumentTerm getTerm () {
+        return termId.getTerm();
+    }
+
+    @Embeddable
+    private static class TermId implements Serializable {
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "term")
+        private DocumentTerm term;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "event_id")
+        private Event event;
+
+        public TermId () {}
+
+        public TermId (DocumentTerm term, Event event) {
+            this.term = term;
+            this.event = event;
+        }
+
+        @Override
+        public boolean equals (Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            TermId termId = (TermId) o;
+            return term.getTerm().equals(termId.term.getTerm()) && event.getId().equals(termId.event.getId());
+        }
+
+        @Override
+        public int hashCode () {
+            return Objects.hash(term.getTerm(), event.getId());
+        }
+
+        public DocumentTerm getTerm () {
+            return term;
+        }
+
+        public Event getEvent () {
+            return event;
+        }
+    }
+}
---
+++ backend/src/main/java/tickr/application/recommendations/RecommenderEngine.java
@@ -0,0 +1,9 @@
+package tickr.application.recommendations;
+
+import tickr.persistence.ModelSession;
+
+public class RecommenderEngine {
+    public static void forceRecalculate (ModelSession session) {
+
+    }
+}
