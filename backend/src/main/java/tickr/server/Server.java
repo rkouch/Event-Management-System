1b2b607027e9a0c5057fca2d4f0da7de92657713
Tom Harrison-Brown <113866206+tomharrisonbrown@users.noreply.github.com>
2022-11-13 09:19:36 +1100

    Added server route for user event recommendations

--- backend/src/main/java/tickr/server/Server.java
+++ backend/src/main/java/tickr/server/Server.java
@@ -108,6 +108,7 @@ public class Server {
         get("/api/group/details", TickrController::groupDetails);
 
         get("/api/recommendations/event", TickrController::recommendEventEvent);
+        get("/api/user/recommendations/home", TickrController::recommendUserEvent);
 
         Spark.get("/api/payment/cancel", (req, response) -> {
             var wrapper = new RouteWrapper<>(dataModel, ctx -> {
