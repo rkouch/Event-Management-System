69e1c02e9846f486fc6935998bac2c3b2be5db72
KennanWong <wongkennan@gmail.com>
2022-11-17 21:05:54 +1100

    clean up spotify

--- frontend/src/Components/SpotifyPlayer.jsx
+++ frontend/src/Components/SpotifyPlayer.jsx
@@ -3,13 +3,8 @@ import { Box } from '@mui/system';
 import React from 'react'
 import { alpha } from '@mui/system';
 
-const CLIENT_ID = '7fe8e700da1947a88047f156715ab612';
-const CLIENT_SECRET = 'a508ed7ca9cc45529d3f62e805b3d34a';
-const REDIRECT_URI = 'http://localhost:3000/'
-
-
-const verifyPlaylistExists = (setError, pathname) => {
-}
+const CLIENT_ID = '7fe8e700da1947a88047f156715ab612'
+const CLIENT_SECRET = 'a508ed7ca9cc45529d3f62e805b3d34a'
 
 export default function SpotifyPlayer({link, editable=false}) {
 
